3b7bae07f26a87a9a0d4101e2e816871316a040f
Langlois Clément <langlois.clement@gmail.com>
2022-12-01 22:47:43 +0100

    Ajoutez des fichiers projet.

---
+++ AoC2022/Program.cs
@@ -0,0 +1,14 @@
+﻿using AoC2022.script;
+using System;
+
+namespace AoC2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Advent of Code!");
+            Console.WriteLine(Day1.part1("../../../input/inputD1.txt"));
+        }
+    }
+}
---
+++ AoC2022/script/Day1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace AoC2022.script
+{
+    class Day1
+    {
+
+        public static int part1(string addr)
+        {
+            if (!File.Exists(addr)) return -1;
+            List<List<int>> listElfe = new List<List<int>>
+            {
+                new List<int>()
+            };
+            using (StreamReader file = new StreamReader(addr))
+            {
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    if (ln == "") listElfe.Add(new List<int>());
+                    else
+                    {
+                        int item = Int32.Parse(ln);
+                        listElfe[^1].Add(item);
+                    }
+                }
+            }
+            List<int> listPoids = listElfe.ConvertAll(x => x.Aggregate(0, (total, next) => total + next));
+            return listPoids.Max();
+        }
+    }
+}
